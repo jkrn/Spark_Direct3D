278d3b8268c13a52ce1f4f512fc0250ac22ba0a1
jkrn <111745714+jkrn@users.noreply.github.com>
2023-12-31 06:25:00 +0100

    Delete source_code directory

--- source_code/Spark/meshes/convert.bat
+++
@@ -1,6 +0,0 @@
-ECHO OFF
-convert_obj_to_binmesh -o checkpointMesh.obj -b checkpoint.binmesh
-convert_obj_to_binmesh -o circlePlatformMesh.obj -b circlePlatform.binmesh
-convert_obj_to_binmesh -o playerMesh.obj -b player.binmesh
-convert_obj_to_binmesh -o skyMesh.obj -b sky.binmesh
-convert_obj_to_binmesh -o upwindMesh.obj -b upwindMesh.binmesh
